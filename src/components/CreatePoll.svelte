ae1ba3235e6432d942283a9528c2a0457dee34ed
Bruce Wayne <brucewayne@mail.com>
2022-01-05 16:26:33 -0500

    Form validation

--- src/components/CreatePoll.svelte
+++ src/components/CreatePoll.svelte
@@ -1,14 +1,40 @@
 <script>
    import Button from "../shared/Button.svelte";
 
-   let fields = {
-      question: "",
-      answerA: "",
-      answerB: "",
-   };
+   let fields = { question: "", answerA: "", answerB: "" };
+   let errors = { question: "", answerA: "", answerB: "" };
+   let valid = false;
 
    const submitHandler = () => {
-      console.log(fields);
+      valid = true;
+
+      // Question validation
+      if (fields.question.trim().length < 5) {
+         valid = false;
+         errors.question = "Question must be at least 5 characters long";
+      } else {
+         errors.question = "";
+      }
+
+      // Answer A validation
+      if (fields.answerA.trim().length < 1) {
+         valid = false;
+         errors.answerA = "Answer A can not be empty";
+      } else {
+         errors.answerA = "";
+      }
+
+      // Answer B validation
+      if (fields.answerB.trim().length < 1) {
+         valid = false;
+         errors.answerB = "Answer A can not be empty";
+      } else {
+         errors.answerB = "";
+      }
+
+      if (valid) {
+         console.log("valid: ", fields);
+      }
    };
 </script>
 
@@ -16,14 +42,17 @@
    <div class="field">
       <label for="question">Poll Quesiton:</label>
       <input type="text" id="question" bind:value={fields.question} />
+      <div class="error">{errors.question}</div>
    </div>
    <div class="field">
       <label for="question">Answer A:</label>
       <input type="text" id="answer-a" bind:value={fields.answerA} />
+      <div class="error">{errors.answerA}</div>
    </div>
    <div class="field">
       <label for="question">Answer B:</label>
       <input type="text" id="answer-b" bind:value={fields.answerB} />
+      <div class="error">{errors.answerB}</div>
    </div>
    <Button type="secondary" flat={true} inverse={true}>Add Poll</Button>
 </form>
@@ -44,5 +73,10 @@
    label {
       margin: 10px auto;
       text-align: left;
+   }
+   .error {
+      font-weight: bold;
+      font-size: 12px;
+      color: #d91b42;
    }
 </style>
